089c985076971febc0f3a647870bd2cfc7943764
Junior <kuakuvidesire@gmail.com>
2020-10-14 23:03:07 +0200

    feat(notes): get the sum of all compute notes

---
+++ Javascript/exercice-7/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+let notes = [10, 13, 13, 12, 15, 12, 11, 16, 14]
+computeNotes(notes);
+
+function computeNotes(notes){
+  let count = 0;
+   for(let i = 0; i < notes.length; i++){
+    count += parseFloat(notes[i], 20);
+    }
+
+    var result = count/notes.length
+    document.write("La moyenne générale est de : " + result);
+}
